eabf739e16472e53ce3029aee2c2c35583c2a479
rashad al shukh <rashad000089@gmail.com>
2024-09-25 01:23:54 +0300

    add search

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,35 +14,50 @@ use App\Models\user;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $data = book::all();
-        return view('home.index',compact('data'));
+        return view('home.index', compact('data'));
     }
 
-    public function book_details($id){
+    public function book_details($id)
+    {
         $data = book::all();
-        return view('home.book_details',compact('data'));
+        return view('home.book_details', compact('data'));
     }
-    public function borrow_books(Request $request, $id) {
+    public function borrow_books(Request $request, $id)
+    {
         $data = book::find($id);
         $qr = $data->quantity;
-    
+
         if ($qr >= '1') {
             if (Auth::id()) {
                 $user_id = Auth::user()->id;
-    
+
                 $borrow = new Borrow;
                 $borrow->book_id = $id;
                 $borrow->user_id = $user_id;
                 $borrow->start_date = $request->start_date;
                 $borrow->end_date = $request->end_date;
                 $borrow->save();
-    
+
                 return redirect()->back()->with('message', 'Request sent to Library Manager');
             }
         } else {
             return redirect()->back()->with('message', 'The book is not available right now');
         }
     }
-    
+    public function explore()
+    {
+        $data = book::all();
+        return view('home.explore', compact('data'));
+    }
+    public function search(Request $re)
+    {
+        $se = $re->search;
+        $data = book::where('title','like','%'.$se.'%')->orWhere('auther_name','like','%'.$se.'%')->get();
+        return view('home.explore', compact('data'));
+
+        
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,36 +5,34 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
 
-route::get('/', [HomeController::class,'index']);
+route::get('/', [HomeController::class, 'index']);
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
-    'verified', 
+    'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
 
-    route::get('/home',[AdminController::class,'index']);
-    route::get('/category_page',[AdminController::class,'category_page']);
-    route::post('/add_category',[AdminController::class,'add_category']);
-    route::get('/cat_delete/{id}',[AdminController::class,'cat_delete']);
-    route::get('/edit_delete/{id}',[AdminController::class,'edit_category']);
-    route::post('/update_category/{id}',[AdminController::class,'update_category']);
-    route::get('/add_book',[AdminController::class,'add_book']);
-    route::get('/show_books',[AdminController::class,'show_books']);
-    route::post('/store_book',[AdminController::class,'store_book']);
-    route::get('/book_delete/{id}',[AdminController::class,'book_delete']);
-    route::get('/edit_book/{id}',[AdminController::class,'edit_book']);
-    route::post('/update_book/{id}',[AdminController::class,'update_book']);
-    route::get('/book_details/{id}',[HomeController::class,'book_details']);
+    route::get('/home', [AdminController::class, 'index']);
+    route::get('/category_page', [AdminController::class, 'category_page']);
+    route::post('/add_category', [AdminController::class, 'add_category']);
+    route::get('/cat_delete/{id}', [AdminController::class, 'cat_delete']);
+    route::get('/edit_delete/{id}', [AdminController::class, 'edit_category']);
+    route::post('/update_category/{id}', [AdminController::class, 'update_category']);
+    route::get('/add_book', [AdminController::class, 'add_book']);
+    route::get('/show_books', [AdminController::class, 'show_books']);
+    route::post('/store_book', [AdminController::class, 'store_book']);
+    route::get('/book_delete/{id}', [AdminController::class, 'book_delete']);
+    route::get('/edit_book/{id}', [AdminController::class, 'edit_book']);
+    route::post('/update_book/{id}', [AdminController::class, 'update_book']);
+    route::get('/book_details/{id}', [HomeController::class, 'book_details']);
     route::match(['get', 'post'], '/borrow_books/{id}', [HomeController::class, 'borrow_books']);
-    route::get('/borrow_request',[AdminController::class,'borrow_request']);
-    route::get('/accept_borrow/{id}',[AdminController::class,'accept_borrow']);
-    route::get('/deny_borrow/{id}',[AdminController::class,'deny_borrow']);
-
-
-
+    route::get('/borrow_request', [AdminController::class, 'borrow_request']);
+    route::get('/accept_borrow/{id}', [AdminController::class, 'accept_borrow']);
+    route::get('/deny_borrow/{id}', [AdminController::class, 'deny_borrow']);
+    route::get('/explore', [HomeController::class, 'explore']);
+    route::get('/search', [HomeController::class, 'search']);
 });
-
